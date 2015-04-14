30089fe37bde1150ce34445e9fbdbfa862800434
ykyoon <xyzlast@gmail.com>
2015-04-14 13:43:14 +0900

    add express-zip

--- app/controllers/system.js
+++ app/controllers/system.js
@@ -3,6 +3,9 @@ var systemService = require('../services/systemservice.js');
 var jsonUtil = require('../utils/jsonutil.js');
 var authUtil = require('../utils/authutil.js');
 var async = require('async');
+var config = require('../../config/config.js');
+var fs = require('fs');
+var zip = require('express-zip');
 
 module.exports = function (app) {
   app.use('/api/system', router);
@@ -14,35 +17,49 @@ var downloadJsonString = function (res, filename, jsonObj) {
   res.end(jsonString);
 };
 
+var writeJsonToFile = function (filename, jsonObj, callback) {
+  var jsonString = JSON.stringify(jsonObj);
+  fs.writeFile(config.root + filename, jsonString, function (err) {
+    callback(null, config.root + '/' + filename);
+  });
+};
+
 router.get('/backup.json', function (req, res) {
   var exportUsers = function (callback) {
     systemService.exportUsers(function (users) {
-      callback(null, users);
+      writeJsonToFile('users.json', users, callback);
     });
   };
   var exportArena = function (callback) {
     systemService.exportArena(function (matches) {
-      callback(null, matches);
+      writeJsonToFile('arenaMatches.json', matches, callback);
     });
   };
   var exportComments = function (callback) {
     systemService.exportComments(function (comments) {
-      callback(null, comments);
+      writeJsonToFile('comments.json', comments, callback);
     });
   };
   var exportLikeLogs = function (callback) {
     systemService.exportLikeLogs(function (logs) {
-      callback(null, logs);
+      writeJsonToFile('likeLogs.json', logs, callback);
     });
   };
   var exportHeroes = function (callback) {
     systemService.exportHeroes(function (heroes) {
-      callback(null, heroes);
+      writeJsonToFile('heroes.json', heroes, callback);
     });
   };
 
   var completedCallback = function (err, results) {
-    downloadJsonString(res, 'backup.json', results);
+    res.zip([
+      { path: results.users, name: 'user.json' },
+      { path: results.arena, name: 'arena.json' },
+      { path: results.likelog, name: 'likelog.json' },
+      { path: results.heroes, name: 'heroes.json' },
+      { path: results.comments, name: 'comments.json' },
+    ]);
+    // downloadJsonString(res, 'backup.json', results);
   };
 
   async.parallel({
